ebaa00c5a26247e14437b2cf8e20318bec0ebd7d
MIMIezno <126945348+MIMIezno@users.noreply.github.com>
2023-04-28 02:28:02 +0800

    Add files via upload

---
+++ Nabua_Final_Project/src/main/java/com/csit228g3/nabua/nabua_final_project/MySQLConnection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.csit228g3.nabua.nabua_final_project;
+
+/**
+ *
+ * @author RONNA
+ */
+public class MySQLConnection {
+    
+}
